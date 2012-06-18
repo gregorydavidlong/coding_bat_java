a3162b85bf4c02503bc0dada2c9f62bfec27a5be
Gregory Long <gregorydavidlong@gmail.com>
2012-06-18 19:09:19 +1000

    Solved groupSum6

--- coding_bat/src/coding/recursion2/Recursion2.java
+++ coding_bat/src/coding/recursion2/Recursion2.java
@@ -16,5 +16,54 @@ public class Recursion2 {
 		}
 		return result;
 	}
+	
+//	private static int count6(int start, int[] nums) {
+//		int count = 0;
+//		for (int x = start; x < nums.length; x++) {
+//			if (nums[x] == 6) {
+//				count++;
+//			}
+//		}
+//		return count;
+//	}
+//
+//	public static boolean groupSum6(int start, int[] nums, int target) {
+//		boolean result = false;
+//		
+//		// base case
+//		if (start == nums.length) {
+//			result = target == 0;
+//		} else if (start == nums.length - 1) {
+//			result = (nums[start] == target) || (target == 0);
+//		} else {
+//			// recursion case
+//			int extraSix = count6(start + 1, nums) * 6;
+//			result = groupSum6(start + 1, nums, target - nums[start] - extraSix) ||
+//					groupSum6(start + 1, nums, target - extraSix);
+//		}
+//		
+//		return result;
+//	}
 
+	
+	public static boolean groupSum6(int start, int[] nums, int target) {
+		
+		// base case
+		if (start == nums.length) {
+			return target == 0;
+		}
+		
+		// recursive case
+		if (nums[start] == 6) {
+			// We must use the 6
+			return groupSum6(start + 1, nums, target - 6);
+		} else {
+			return (groupSum6(start + 1, nums, target - nums[start])) 
+					|| 
+					groupSum6(start + 1, nums, target);
+			
+		}
+		
+	}
+	
 }
--- coding_bat/test/coding/recursion2/Recursion2Test.java
+++ coding_bat/test/coding/recursion2/Recursion2Test.java
@@ -37,5 +37,37 @@ public class Recursion2Test {
 		assertFalse(Recursion2.groupSum(0, nums, 4));
 		assertFalse(Recursion2.groupSum(1, nums, 1));
 	}
+	
+	@Test
+	public void groupSum6Examples() {
+		int[] nums = {5, 6, 2};
+		assertTrue(Recursion2.groupSum6(0, nums, 8));
+		assertFalse(Recursion2.groupSum6(0, nums, 9));
+		assertFalse(Recursion2.groupSum6(0, nums, 7));
+	}
+	
+	@Test 
+	public void groupSum6EmptyList() {
+		int[] nums = {};
+		assertTrue(Recursion2.groupSum6(0, nums, 0));
+		assertFalse(Recursion2.groupSum6(0, nums, 1));
+	}
+	
+	@Test
+	public void groupSum6SingleElement() {
+		int[] nums = {6};
+		assertTrue(Recursion2.groupSum6(0, nums, 6));
+		assertFalse(Recursion2.groupSum6(0, nums, 3));
+	}
+	
+	@Test 
+	public void groupSum6TwoElements() {
+		int[] nums = {1, 6};
+		assertFalse(Recursion2.groupSum6(0, nums, 1));
+		assertTrue(Recursion2.groupSum6(0, nums, 6));
+		assertTrue(Recursion2.groupSum6(0, nums, 7));
+		assertFalse(Recursion2.groupSum6(0, nums, 4));
+		assertFalse(Recursion2.groupSum6(1, nums, 1));
+	}
 
 }
